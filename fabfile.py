8630f36092f91933b7e1e51082d75133f1f6c449
Taylor Nichols <taylornichols@ymail.com>
2012-11-01 21:49:19 -0400

    Committing production repo

--- fabfile.py
+++ fabfile.py
@@ -3,10 +3,11 @@ from fabric.api import local, lcd
 apps = ('article',)
 
 def prepare_deployment(branch_name):
+    print 'preparing deployment for %s' % branch_name
     for appname in apps:
         local('python manage.py test %s' % appname)
     local('git add -p && git commit')
-    local('git checkout master && git merge ' + branch_name)
+    local('git checkout master && git merge %s' % branch_name)
     
 def deploy():
     with lcd('~/Documents/crimsonbeacon/crimsonbeacon'):
