498c3efb19ee976bc8ebf36b14ed245f06f2473e
Taylor Nichols <taylornichols@ymail.com>
2012-11-13 00:15:53 -0500

    Committing files before pushing to github repository as epitaphaudio

--- apps/survey/urls.py
+++ apps/survey/urls.py
@@ -1,5 +1,5 @@
 from apps.survey.views import SurveyListView, SurveyDetailView, SurveyCreateView, \
-    SurveyUpdateView
+    SurveyUpdateView, SurveyDeleteView
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',   
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^$', SurveyListView.as_view(), name='survey_list'),        
     url(r'^(?P<title>[a-z0-9_-]{1,50})$', SurveyDetailView.as_view(), name='survey_detail'),
     url(r'^(?P<title>[a-z0-9_-]{1,50})/update/$', SurveyUpdateView.as_view(), name='survey_update'),
+    url(r'^(?P<title>[a-z0-9_-]{1,50})/delete/$', SurveyDeleteView.as_view(), name='survey_delete'),
 )
 
 
--- apps/survey/views.py
+++ apps/survey/views.py
@@ -1,9 +1,9 @@
-from apps.survey.forms import SurveyCreateForm,SurveyUpdateForm
+from apps.survey.forms import SurveyCreateForm, SurveyUpdateForm
 from apps.survey.models import Survey
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 
 class SurveyDetailView(DetailView):
@@ -38,22 +38,37 @@ class SurveyCreateView(CreateView):
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_staff:
             return super(SurveyCreateView, self).dispatch(request, *args, **kwargs)
-        return HttpResponseRedirect(reverse_lazy('survey_list'))
+        return HttpResponseRedirect(re/vervweftest-survey-4/delete/verse_lazy('survey_list'))
     
 class SurveyUpdateView(UpdateView):
     
     form_class = SurveyUpdateForm
     model = Survey
     template_name = 'survey/templates/survey_update.html'
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_staff:
+            return super(SurveyUpdateView, self).dispatch(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('survey_detail', kwargs={'title': kwargs['title']}))
+    
+    def get_object(self):
+        survey = self.get_queryset().get(title=self.kwargs['title'].replace('-', ' '))
+        return survey
+    
+class SurveyDeleteView(DeleteView):
+    
+    model = Survey
+    template_name = 'survey/templates/survey_delete.html'
     success_url = reverse_lazy('survey_list')
     
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_staff:
-            return super(SurveyCreateView, self).dispatch(request, *args, **kwargs)
+            return super(SurveyDeleteView, self).dispatch(request, *args, **kwargs)
         return HttpResponseRedirect(reverse_lazy('survey_list'))
     
     def get_object(self):
-        survey = self.get_queryset().get(title=self.kwargs['title'])
+        survey = self.get_queryset().get(title=self.kwargs['title'].replace('-', ' '))
         return survey
+        
     
     
